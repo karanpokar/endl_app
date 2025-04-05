958fc16e976583f9011ae08e6acbd9ff7caf1249
Karan <karanpatel2199@gmail.com>
2025-04-05 11:48:03 +0530

    App Name Updated

--- android/app/src/main/java/com/endlapp/MainActivity.kt
+++ android/app/src/main/java/com/endlapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.helloworld
+package com.endlapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "HelloWorld"
+  override fun getMainComponentName(): String = "EndlApp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/endlapp/MainApplication.kt
+++ android/app/src/main/java/com/endlapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.helloworld
+package com.endlapp
 
 import android.app.Application
 import com.facebook.react.PackageList
