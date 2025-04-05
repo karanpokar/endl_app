ff4e291700fb9de451f29e3ebb13746127b70313
Karan <karanpatel2199@gmail.com>
2025-04-05 14:45:49 +0530

    gitignore updated

---
+++ android/app/.cxx/RelWithDebInfo/6t1rh3m4/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/apple/.gradle/caches/8.13/transforms/286c28cf36d8812cec85e16101adcab0/transformed/hermes-android-0.76.9-release/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/apple/.gradle/caches/8.13/transforms/286c28cf36d8812cec85e16101adcab0/transformed/hermes-android-0.76.9-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
